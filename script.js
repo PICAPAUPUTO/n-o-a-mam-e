524de2048d8f53f12b7c00e3f29deb470c3d4f24
Caio Candido <149337906+PICAPAUPUTO@users.noreply.github.com>
2023-11-07 11:01:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+alert("não é a mamãe")
+
+document.getElementById("texto").innerHTML="só aprende a beijar beijando";
+
+var titulo = document.querySelector("p")
+titulo.textContent="tecnologia da informação";
+
+let nome = "Samyr";
+
+if(nome == Samyr){
+    console.log("aluno de T.I")
+}else{
+    console.log("não é aluno de T.I")
+}
